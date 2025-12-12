a499a039ba0d1a8a3e8793f0914b715ef2995149
jbuto454 <jakebuto@pm.me>
2025-12-11 22:02:29 -0500

    added comments and cleaned up the code

--- backend/app.py
+++ backend/app.py
@@ -18,34 +18,39 @@ def create_app() -> Flask:
         response.headers["Access-Control-Allow-Headers"] = "Content-Type"
         return response
 
+    #make the solution easier to read for the user
+    #ideally this would be done on the front-end, but for the purposes of this exercise I am doing this on the backend
     def pretty_solution(solution):
-
+        #initialize a string to return
         return_string = ""
-
+        #loop through the solution list
         for i in range(0, len(solution)):
-
+            #loop through each inner dictionary and extract the variable and associated value
             for var, val in solution[i].items():
+                #concatenate the variable and simplify the associated value and round it if needed
                 return_string += str(var) + " = " + str(round(val.evalf(), 4)) + "\n"
 
         return return_string
 
+    #cleans the user input to account for common equation input error
+    #for example, handles 'x2' and converts it to 'x*2'
+    #also handles 'x^2' and converts it to 'x**2'
+    def clean_user_input(equation):
 
-    @app.get("/solve")
-    def solve_equation():
-        equation = (request.args.get("equation") or "").strip()
-        print(str(equation), flush=True)
-        if not equation:
-            return jsonify({"error": "Missing 'equation' query parameter"}), 400
-
+        #initialize all variables
         clean_equation = ""
         previous_value = ""
         has_equals = False
         left_side = ""
         right_side = ""
 
+        #loops through the user input and fix common syntax issues
         for i in range(0, len(equation)):
+            #for all char values besides '=', check if the user entered a value like 'x2' or '2x'
             if equation[i] != "=":
-                if (equation[i].isnumeric() & previous_value.isalpha()) | (equation[i].isalpha() & previous_value.isnumeric()):
+                if (equation[i].isnumeric() & previous_value.isalpha()) | (
+                        equation[i].isalpha() & previous_value.isnumeric()):
+                    #if the user entered a value like 'x2' or '2x', then add a '*' between the chars
                     clean_equation += "*"
                 elif equation[i] == "^":
                     clean_equation += "*"
@@ -61,6 +66,14 @@ def create_app() -> Flask:
                 clean_equation = ""
                 previous_value = ""
 
+        return clean_equation, has_equals, left_side, right_side
+
+
+    #extracts the expression object from the equation that was passed
+    def extract_expression(equation):
+
+        clean_equation, has_equals, left_side, right_side = clean_user_input(equation)
+
         if has_equals:
             right_side = clean_equation
             try:
@@ -68,18 +81,31 @@ def create_app() -> Flask:
                 print(right_side)
                 expr = Eq(sympify(left_side), sympify(right_side))
             except Exception as e:
-                print(f"error type: {type(e)}", flush=True)
-                print(f"error message: {e}", flush=True)
-                return jsonify({"error": str(e)}), 400
+                return None, e
 
         else:
             try:
-                #convert the string into a sympy expression
+                # convert the string into a sympy expression
                 expr = sympify(clean_equation)
             except Exception as e:
-                print(f"error type: {type(e)}", flush=True)
-                print(f"error message: {e}", flush=True)
-                return jsonify({"error": str(e)}), 400
+                return None, e
+
+        return expr
+
+
+    @app.get("/solve")
+    def solve_equation():
+        equation = (request.args.get("equation") or "").strip()
+        print(str(equation), flush=True)
+        if not equation:
+            return jsonify({"error": "Missing 'equation' query parameter"}), 400
+
+        expr, e = extract_expression(equation)
+
+        if expr is None:
+            print(f"error type: {type(e)}", flush=True)
+            print(f"error message: {e}", flush=True)
+            return jsonify({"error": str(e)}), 400
 
         print("converted to expr")
 
